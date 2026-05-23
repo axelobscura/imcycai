42441f27b173068e09c6f725d7ef434203b3f55c
Axel Laurent <axosar@gmail.com>
2026-05-23 09:37:29 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,36 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="relative flex min-h-full w-full flex-1 flex-col items-center justify-center font-sans">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/bkg.jpg')" }}
+        aria-hidden="true"
+      />
+      <div
+        className="absolute inset-0 bg-linear-to-br from-zinc-800/80 via-stone-900/70 to-neutral-950/90"
+        aria-hidden="true"
+      />
+      <div className="relative z-10 flex flex-col items-center justify-center bg-white/10 backdrop-blur-sm rounded-lg shadow-lg p-20 px-20 max-w-md">
+        <h1 className="text-4xl font-light text-white mb-3 text-center">
+          BIENVENIDO A LA PLATAFORMA INTELIGENTE DEL CONCRETO
+        </h1>
+        <p className="text-sm text-white font-light mb-5">
+          Instituto Mexicano del Cemento y del Concreto A.C.
+        </p>
+        <input
+          type="text"
+          placeholder="Email"
+          className="w-full p-4 rounded-md mb-3 border-2 border-slate-500 bg-slate-800/80 text-white"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-4 rounded-md mb-3 border-2 border-slate-500 bg-slate-800/80 text-white"
+        />
+        <button className="w-full p-4 rounded-md bg-blue-500 text-white">
+          INGRESAR
+        </button>
+      </div>
     </div>
   );
 }
