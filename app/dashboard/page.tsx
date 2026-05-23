5d6fce5e3e0764cbfe817f9c9be45b41a50f37d6
Axel Laurent <axosar@gmail.com>
2026-05-23 11:32:11 -0600

    logo

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <div className="relative flex min-h-full w-full flex-1 flex-col items-center justify-center font-sans">
+      <div
+        className="absolute inset-0 bg-linear-to-br from-zinc-800/80 via-stone-900/70 to-neutral-950/90"
+        aria-hidden="true"
+      />
+      <div className="relative z-10 flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm rounded-lg shadow-lg p-20 px-10 max-w-md">
+        <h1 className="text-4xl font-light text-white mb-3 text-center">
+          DASHBOARD
+        </h1>
+        
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="relative flex min-h-full w-full flex-1 flex-col items-center justify-center font-sans">
@@ -10,13 +13,14 @@ export default function Home() {
         className="absolute inset-0 bg-linear-to-br from-zinc-800/80 via-stone-900/70 to-neutral-950/90"
         aria-hidden="true"
       />
-      <div className="relative z-10 flex flex-col items-center justify-center bg-white/10 backdrop-blur-sm rounded-lg shadow-lg p-20 px-20 max-w-md">
-        <h1 className="text-4xl font-light text-white mb-3 text-center">
-          BIENVENIDO A LA PLATAFORMA INTELIGENTE DEL CONCRETO
-        </h1>
-        <p className="text-sm text-white font-light mb-5">
+      <div className="relative z-10 flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm rounded-lg shadow-lg p-20 px-10 max-w-md">
+        <Image src="/logo-imcyc.svg" alt="Logo" width={300} height={200} className="mb-5" />
+        <p className="text-sm text-white font-light mb-10 text-center">
           Instituto Mexicano del Cemento y del Concreto A.C.
         </p>
+        <h1 className="text-4xl font-light text-white mb-3 text-center">
+          BIENVENIDO A LA PLATAFORMA INTELIGENTE DEL CEMENTO Y EL CONCRETO
+        </h1>
         <input
           type="text"
           placeholder="Email"
@@ -27,9 +31,9 @@ export default function Home() {
           placeholder="Password"
           className="w-full p-4 rounded-md mb-3 border-2 border-slate-500 bg-slate-800/80 text-white"
         />
-        <button className="w-full p-4 rounded-md bg-blue-500 text-white">
-          INGRESAR
-        </button>
+        <Link href="/dashboard" className="w-full p-4 rounded-md bg-blue-500 text-white block text-center">
+          <span className="text-center font-bold text-lg">INGRESAR</span>
+        </Link>
       </div>
     </div>
   );
