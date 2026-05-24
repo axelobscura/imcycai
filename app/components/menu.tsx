426b3756997629723dd2b632206365e6cc5de89a
Axel Laurent Obscura Sarzotti <axel@Axels-MacBook-Pro.local>
2026-05-24 09:55:07 -0600

    rounded

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -3,7 +3,7 @@ import { FaFistRaised } from "react-icons/fa";
 
 export default function Menu() {
     return (
-      <div className="flex justify-center items-center min-h-screen w-full flex-1 flex-col items-center justify-center font-sans ml-3">
+      <div className="flex justify-center items-center min-h-screen w-full flex-1 flex-col items-center justify-center font-sans ml-3 rounded-tab-left">
         <div className="flex justify-center items-center rounded-lg p-4 w-full">
             <ul>
               <li>
